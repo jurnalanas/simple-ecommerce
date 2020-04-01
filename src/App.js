5210b32440fa70155a2d1eef398686945a3c85eb
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2020-04-01 17:01:02 +0700

    added npm scripts, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App" >
-        <div className="w-full max-w-md bg-gray-800" >
-          <form action="" className=" bg-white shadow-md rounded px-8 py-8 pt-8">
-            <div className="px-4 pb-4">
-              <label htmlFor="email" className="text-sm block font-bold  pb-2">EMAIL ADDRESS</label>
-              <input type="email" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300 " placeholder="Johnbull@example.com"/>
-            </div>
-            <div  className="px-4 pb-4">
-              <label htmlFor="password" className="text-sm block font-bold pb-2">PASSWORD</label>
-              <input type="password" name="email" id="" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline border-blue-300" placeholder="Enter your password"/>
-            </div>
-            <div>
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Sign In</button>
-            </div>
-          </form>
-        </div>
+    <div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src={logo} alt="ChitChat Logo" />
       </div>
+      <div className="ml-6 pt-1">
+        <h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
+        <p className="text-base text-gray-600 leading-normal">
+          You have a new message!
+        </p>
+      </div>
+    </div>
   );
 }
 
