38fbcfaabed88da21e00988a90734595c680ac2f
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2020-04-02 06:02:55 +0700

    improve post production

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,22 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.js',
+    './public/index.html',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
     require('autoprefixer'),
-  ],
-};
+    ...process.env.NODE_ENV === 'production' ?
+    [purgecss] :
+    []
+  ]
+}
