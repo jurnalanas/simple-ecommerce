290b3fd98a8f89ae57013d4f20d14663130fb64f
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2020-04-02 20:08:12 +0700

    init categories

--- src/products/pages/Products.js
+++ src/products/pages/Products.js
@@ -2,14 +2,15 @@ import React from "react";
 import Navbar from "../../shared/components/Navigation/Navbar";
 import Contents from "../components/Contents";
 import FloatingBar from "../../shared/components/Navigation/FloatingBar";
-import CategoriesComponent from "../../shared/components/UIElements/Categories";
+import Categories from "../../shared/components/UIElements/Categories";
+
 
 function Products() {
   return (
     <>
       <div>
         <Navbar />
-        <CategoriesComponent/>
+        <Categories/>
         <div className="container max-w-xl mx-auto">
           <div className="px-4 mb-16 text-gray-800 leading-normal w-">
             <Contents />
--- src/shared/components/UIElements/Categories.js
+++ src/shared/components/UIElements/Categories.js
@@ -1,67 +1,52 @@
 import React from 'react';
 
+const CATEGORIES_ITEM = [
+    {
+      "imageUrl": "https://img.icons8.com/bubbles/2x/t-shirt.png",
+      "id": 21,
+      "name": "Baju"
+    },
+    {
+      "imageUrl": "https://img.icons8.com/flat_round/2x/long-shorts.png",
+      "id": 42,
+      "name": "Celana"
+    },
+    {
+      "imageUrl": "https://img.icons8.com/flat_round/2x/summer-hat.png",
+      "id": 11,
+      "name": "Topi"
+    },
+    {
+      "imageUrl": "https://img.icons8.com/color/2x/red-purse.png",
+      "id": 91,
+      "name": "Tas"
+    },
+    {
+      "imageUrl": "https://img.icons8.com/cute-clipart/2x/apple-watch-apps.png",
+      "id": 131,
+      "name": "Jam Tangan"
+    }
+  ];
+
 const Categories = () => {
   return (
     <div className="bg-white border-b-2">
       <div className="max-w-xl mx-auto overflow-hidden">
-        <div className="bg-white inline-block mx-2 overflow-x-scroll p-2 text-center text-gray-700 text-sm w-full whitespace-no-wrap">
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-          <div className="align-middle inline-block overflow-hidden rounded mx-4">
-            <img
-              style={{width: '30px'}}
-              src="http://icons.iconarchive.com/icons/treetog/i/128/Floppy-Small-icon.png"
-              alt="Sunset in the mountains"
-            />
-            <div>two</div>
-          </div>
-
+        <div className="bg-white flex mx-2 overflow-x-scroll p-2 text-center text-gray-700 text-sm w-full whitespace-no-wrap">
+          { CATEGORIES_ITEM.map((item) => (
+            <div
+              className="align-middle inline-block overflow-hidden rounded mx-4 flex-1"
+              key={item.id}
+            >
+              <img
+                className="m-auto"
+                style={{width: '30px'}}
+                src={item.imageUrl}
+                alt={item.name}
+              />
+              <div>{item.name}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
