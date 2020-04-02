e9a1114f8d6f3e4175bee03d744622a54379badf
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2020-04-02 09:36:01 +0700

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Contents from "./components/Contents";
-import FloatingBar from "./components/FloatingBar";
-import CategoriesComponent from "./components/Categories";
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
+import Home from './products/pages/Home';
+import Products from './products/pages/Products';
+
+
+const App = () => {
   return (
-    <>
-      <div>
-        <Navbar />
-        <CategoriesComponent/>
-        <div className="container max-w-xl mx-auto">
-          <div className="px-4 mb-16 text-gray-800 leading-normal w-">
-            <Contents />
-          </div>
-        </div>
-      <FloatingBar/>
-      </div>
-    </>
-  );
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home/>
+        </Route>
+        <Route path="/products" exact>
+          <Products/>
+        </Route>
+        <Redirect to ="/" />
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/products/pages/Products.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Contents from "../../components/Contents";
+import FloatingBar from "../../components/FloatingBar";
+import CategoriesComponent from "../../components/Categories";
+
+function Products() {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <CategoriesComponent/>
+        <div className="container max-w-xl mx-auto">
+          <div className="px-4 mb-16 text-gray-800 leading-normal w-">
+            <Contents />
+          </div>
+        </div>
+      <FloatingBar/>
+      </div>
+    </>
+  );
+}
+
+export default Products;
