36624fd44b78322fa7054a6ed41fd520cf7318c1
Muhammad Nasrurrohman <anas.bladz@gmail.com>
2020-04-05 21:24:51 +0700

    configure cypress

--- cypress/e2e/users.js
+++ cypress/e2e/users.js
@@ -1,10 +1,12 @@
 describe('anonymous users', () => {
   it('can login via social media button', () => {
-    cy
-      .visit('http://localhost:3000')
+    cy.visit('/')
       .get('.bg-blue-500')
       .click()
       .get(':nth-child(1) > .text-gray-800')
       .should('have.text', 'Home')
+  })
+  it('can validate login', () => {
+    cy.visit('/')
   })
 })
